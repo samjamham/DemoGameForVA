366e9fd5636a4b9fc23d672abe99a1a6544d19fb
Sam <Sam_Crook@live.co.uk>
2026-06-16 11:10:33 +0100

    Voice line stopping
    Voice line is now stopped when the UI is closed.

--- Assets/ReplayVoiceLines.cs
+++ Assets/ReplayVoiceLines.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 public class ReplayVoiceLines : MonoBehaviour
 {
     [SerializeField] private GameObject UIelement;
+    [SerializeField] private VoiceLinePlayer VLPlayer;
 
     public void toggleUIState()
     {
+        if (UIelement.activeInHierarchy) 
+        {
+            VLPlayer.StopClip();
+        }
         UIelement.SetActive(!UIelement.activeInHierarchy);
     }
 }
